dfebbbacb767cc31d15db67b964a04b51e7d64b2
aakash <aakashkumar162006@gmail.com>
2026-01-10 13:09:46 +0530

    Made Improvements and added HTTP Exceptions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, Depends
-from models import StockBase,StockCreate,StockRead,StockUpdate, OrderCreate,OrderRead
+from fastapi import FastAPI, Depends, HTTPException
+from models import StockRead, StockCreate, StockUpdate, OrderRead, OrderCreate
 import database_models
 from config import engine, SessionLocal
 from sqlalchemy.orm import Session
-from sqlalchemy import false
 
 api = FastAPI()
 database_models.Base.metadata.create_all(bind = engine)
@@ -43,8 +42,6 @@ def init_db():
             db.add(database_models.Stock(**i.model_dump()))
         db.commit()
     db.close()
-        
-
 init_db()
             
 def get_db():
@@ -54,30 +51,31 @@ def get_db():
     finally:
         db.close()
 
-
 #Stocks
-@api.get("/stocks", response_model = list[StockRead])
+@api.get("/stocks", response_model = list[StockRead], status_code = 200)
 def get_all_stocks(db: Session = Depends(get_db)):
     stocks = db.query(database_models.Stock).all()
-    if stocks:
-        return stocks
-    else:
-        return {"error" : "Stock Not Found"}
+    return stocks
+   
     
 
-@api.get("/stocks/{id}", response_model = StockRead)
+@api.get("/stocks/{id}", response_model = StockRead, status_code = 200)
 def get_stock(id: int, db: Session = Depends(get_db)):
     stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == id).first()
     if stock:
         return stock
+    else:
+        raise HTTPException(status_code = 404, detail = "Stock Not Found")
     
-@api.post("/stocks")
-def add_stock(stock: StockBase, db: Session = Depends(get_db)):
-    db.add(database_models.Stock(**stock.model_dump()))
+@api.post("/stocks",response_model = StockRead,status_code = 201)
+def add_stock(stock: StockCreate, db: Session = Depends(get_db)):
+    created_stock = database_models.Stock(**stock.model_dump())
+    db.add(created_stock)
     db.commit()
-    return {'message' : 'Stock added successfully'}
+    db.refresh(created_stock)
+    return created_stock
 
-@api.put("/stocks/{id}")
+@api.patch("/stocks/{id}", response_model=StockRead, status_code = 200)
 def update_stock(id: int, stock: StockUpdate, db: Session = Depends(get_db)):
     db_stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == id).first()
     if db_stock:
@@ -85,58 +83,61 @@ def update_stock(id: int, stock: StockUpdate, db: Session = Depends(get_db)):
         for field, value in updated_stock.items():
             setattr(db_stock, field, value)
         db.commit()
-        return {'message' : 'Stock updated successfully'}
-    return {'error' : 'Stock not found'}
+        return db_stock
+    raise HTTPException(status_code = 404, detail = "Stock Not Found")
 
-@api.patch("/stocks/{id}/toggle-active")
+@api.patch("/stocks/{id}/toggle-active", response_model = StockRead, status_code = 200)
 def toggle_stock_activity(id: int, db: Session = Depends(get_db)):
     db_stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == id).first()
     if db_stock:
         db_stock.is_active = not db_stock.is_active
         db.commit()
-        return {'message': f'Stock activity toggled to {db_stock.is_active}'}
-    return {'error': 'Stock not found.'}
-
-@api.delete("/stocks/{id}")
-def delete_stock(id: int, db: Session = Depends(get_db)):
-    db_stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == id).first()
-    if db_stock:
-        db.delete(db_stock)
-        db.commit()
-        return {'message' : 'Stock removed successfully'}
-
+        return db_stock
+    else:    
+        raise HTTPException(status_code = 404, detail = "Stock Not Found")
 
 # Order
-@api.get("/orders", response_model = list[OrderRead])
+@api.get("/orders", response_model = list[OrderRead], status_code = 200)
 def get_all_orders(db: Session = Depends(get_db)):
     db_orders = db.query(database_models.Order).all()
-    if db_orders:
-        return db_orders
-    return {'error' : 'Orders not found'}
+    return db_orders
+    
 
-@api.get("/orders/{id}", response_model = OrderRead)
+@api.get("/orders/{id}", response_model = OrderRead, status_code = 200)
 def get_order(id: int, db: Session = Depends(get_db)):
     db_order = db.query(database_models.Order).filter(database_models.Order.order_id == id).first()
     if db_order:
         return db_order
-    return {'error' : 'Order not found'}
+    raise HTTPException(status_code = 404, detail = "Order Not Found")
 
-@api.post("/orders")
+@api.post("/orders", response_model = OrderRead, status_code = 201)
 def add_order(order: OrderCreate, db: Session = Depends(get_db)):
-    db.add(database_models.Order(**order.model_dump()))
-    stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == order.stock_id).first()
-    if stock:
-        if stock.stock_quantity > order.quantity:
-            stock.stock_quantity -= order.quantity
-        elif stock.stock_quantity == order.quantity:
-            stock.stock_quantity -= order.quantity
+    try:
+        stock = db.query(database_models.Stock).filter(database_models.Stock.stock_id == order.stock_id).with_for_update().first()
+        if not stock:
+            raise HTTPException(status_code = 404, detail = "Stock Not Found")
+        
+        if stock.is_active == False:
+            raise HTTPException(status_code = 400, detail = "Stock currently unavailable")
+
+        if stock.stock_quantity < order.quantity:
+            raise HTTPException(status_code = 400, detail = "Order exceeds Stock quantity")
+        
+        stock.stock_quantity -= order.quantity
+        if stock.stock_quantity == 0:
             stock.is_active = False
-        elif stock.is_active == False:
-            return {'message' : 'Stock currently out of stock'}
-        else:
-            return {'message' : 'Order quantity exceeds available stock'}
+        
+        created_order = database_models.Order(**order.model_dump())
+        db.add(created_order) 
         db.commit()
-        return {'message' : 'Order created successfully'}
-    else:
-        return {'error' : 'Stock not found'}
+        db.refresh(created_order)
+        return created_order
+    
+    except HTTPException:
+        db.rollback()
+        raise
+
+    except Exception:
+        db.rollback()
+        raise HTTPException(500, "Order Creation Failed")
 
--- models.py
+++ models.py
@@ -1,14 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 
-class StockBase(BaseModel):
+class StockRead(BaseModel):
+    stock_id: int
     stock_name: str
     stock_price: float
     stock_quantity: int
+    is_active: bool
+    class Config:
+        from_attributes = True
 
-class StockCreate(StockBase):
-    pass
+class StockCreate(BaseModel):
+    stock_name: str
+    stock_price: float
+    stock_quantity: int
 
 class StockUpdate(BaseModel):
     stock_name: Optional[str] = None
@@ -16,22 +22,15 @@ class StockUpdate(BaseModel):
     stock_quantity: Optional[int] = None
     is_active: Optional[bool] = None
 
-class StockRead(StockBase):
-    stock_id: int
-    is_active: bool
-    class Config:
-        from_attributes = True
-
-class OrderCreate(BaseModel):
-    stock_id: int
-    quantity: int
-    
 class OrderRead(BaseModel):
     order_id: int
     stock_id: int
     quantity: int
     status: bool
     created_at: datetime
-
     class Config:
         from_attributes = True
+
+class OrderCreate(BaseModel):
+    stock_id: int
+    quantity: int = Field(gt=0)
